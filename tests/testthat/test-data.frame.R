0cf1d16e72d14d5d325c84678d57a063f67e7852
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-03-27 13:33:42 -0700

    Tests for data.frame

---
+++ tests/testthat/test-data.frame.R
@@ -0,0 +1,13 @@
+context("data.frame")
+
+test_that("basic data.frame", {
+  d <- mtcars
+  it <- iterator(d)
+
+  expect_that(it$length, equals(nrow(d)))
+  expect_that(it$is_complete, is_false())
+  for (i in seq_len(nrow(d))) {
+    expect_that(it$yield(), equals(d[i,]))
+  }
+  expect_that(it$yield(), throws_error("done"))
+})
